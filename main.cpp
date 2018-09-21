a8544b37c1a7185586ba1ac07504611277d4d8dd
JonathanLoza <lozajnt@gmail.com>
2018-09-21 18:29:58 -0500

    counting sort finalizado

--- main.cpp
+++ main.cpp
@@ -13,9 +13,9 @@ int main(int argc, char *argv[]) {
     print(numbers, size);
     countingSort(numbers, size);
     print(numbers, size);
-    
-    system("read");
-    return EXIT_SUCCESS;
+
+    //system("read");
+    return 0;
 }
 
 void print(int numbers[], int size) {
@@ -26,5 +26,24 @@ void print(int numbers[], int size) {
 }
 
 void countingSort(int numbers[], int size) {
-    // TODO
+    int max=0;
+    int num2[size]={0};
+    for (int i = 0; i <size; i++) {
+      num2[i]=numbers[i];
+      if(max<numbers[i]){
+          max=numbers[i];
+      }
+    }
+    int list1[max+1]={0};
+    for (int i = 0; i <size; i++){
+      list1[numbers[i]]++;
+    }
+    for (int i = 1; i <max+1; i++){
+      list1[i]+=list1[i-1];
+
+    }
+    for(int i=0; i<size;i ++){
+      numbers[list1[num2[i]]-1]=num2[i];
+      list1[num2[i]]--;
+    }
 }
